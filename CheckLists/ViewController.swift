db529d929d2ec2da85bb3751d015c7c0c33f7085
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-12 17:29:29 +0800

    First commit

---
+++ CheckLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckLists
+//
+//  Created by Giovanni Gaffé on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
