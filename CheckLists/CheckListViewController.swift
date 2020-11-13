90e67a5173321eca282011be8401418bf52ebb4c
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-13 18:51:48 +0800

    Change name in accord with project name

--- CheckLists/CheckListViewController.swift
+++ CheckLists/CheckListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class CheckListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
