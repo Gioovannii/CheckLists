c785a0f9aa0a6542b7b231ff4bab812ba73f84f4
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-14 10:37:05 +0800

    Set delegate and deselect checkmark when tapped

--- CheckLists/CheckListViewController.swift
+++ CheckLists/CheckListViewController.swift
@@ -38,4 +38,19 @@ class CheckListViewController: UITableViewController {
         }
         return cell
     }
+    
+    // MARK: - Table View Delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+
+    }
 }
