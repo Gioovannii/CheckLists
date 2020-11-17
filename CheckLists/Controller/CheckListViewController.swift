e2785f927d49b30ee3a0c0d922de5720dbce63fa
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-17 18:14:54 +0800

    Add external properties

--- CheckLists/Controller/CheckListViewController.swift
+++ CheckLists/Controller/CheckListViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class CheckListViewController: UITableViewController {
+    let row0text = "Walk the dog"
+    let row1text = "Brush my teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,31 +37,16 @@ class CheckListViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         
         if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
+            label.text = row3text
         } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream "
+            label.text = row4text
         }
         return cell
-    }
-    
-    // MARK: - Table View Delegate
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-
     }
 }
