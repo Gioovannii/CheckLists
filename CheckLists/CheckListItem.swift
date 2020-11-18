ccf03c115a2968e5599630eb9880a0c1656ae089
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-18 20:10:58 +0800

    New file to store data

---
+++ CheckLists/CheckListItem.swift
@@ -0,0 +1,13 @@
+//
+//  CheckListItem.swift
+//  CheckLists
+//
+//  Created by Giovanni Gaffé on 2020/11/18.
+//
+
+import Foundation
+
+final class CheckListItem {
+    var text = ""
+    var checked = false
+}
