fa6ee8521779e778a84b1158e4b2ae88d1985a40
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-19 19:45:18 +0800

    Change all methods by items array

--- CheckLists/Controller/CheckListViewController.swift
+++ CheckLists/Controller/CheckListViewController.swift
@@ -49,26 +49,29 @@ class CheckListViewController: UITableViewController {
     // MARK: - Table view Data Source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
         
+        let item = items[indexPath.row]
         let label = cell.viewWithTag(1000) as! UILabel
-        
-        if indexPath.row == 0 {
-            label.text = row0item.text
-        } else if indexPath.row == 1 {
-            label.text = row1item.text
-        } else if indexPath.row == 2 {
-            label.text = row2item.text
-        } else if indexPath.row == 3 {
-            label.text = row3item.text
-        } else if indexPath.row == 4 {
-            label.text =  row4item.text
-        }
+        label.text = item.text
         configureCell(for: cell, at: indexPath)
+//
+//        if indexPath.row == 0 {
+//            label.text = row0item.text
+//        } else if indexPath.row == 1 {
+//            label.text = row1item.text
+//        } else if indexPath.row == 2 {
+//            label.text = row2item.text
+//        } else if indexPath.row == 3 {
+//            label.text = row3item.text
+//        } else if indexPath.row == 4 {
+//            label.text =  row4item.text
+//        }
+//        configureCell(for: cell, at: indexPath)
         return cell
     }
     // MARK: - Table View Delegate
@@ -76,45 +79,59 @@ class CheckListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if let cell = tableView.cellForRow(at: indexPath) {
-            if indexPath.row == 0 {
-                row0item.checked.toggle()
-                
-            } else if indexPath.row == 1 {
-                row1item.checked.toggle()
-                
-            } else if indexPath.row == 2 {
-                row2item.checked.toggle()
-                
-            } else if indexPath.row == 3 {
-                row3item.checked.toggle()
-                
-            } else if indexPath.row == 4 {
-                row4item.checked.toggle()
-            }
+            var item = items[indexPath.row]
+            item.checked.toggle()
+            
             configureCell(for: cell, at: indexPath)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+//            if indexPath.row == 0 {
+//                row0item.checked.toggle()
+//
+//            } else if indexPath.row == 1 {
+//                row1item.checked.toggle()
+//
+//            } else if indexPath.row == 2 {
+//                row2item.checked.toggle()
+//
+//            } else if indexPath.row == 3 {
+//                row3item.checked.toggle()
+//
+//            } else if indexPath.row == 4 {
+//                row4item.checked.toggle()
+//            }
+//            configureCell(for: cell, at: indexPath)
+//        }
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
     
     func configureCell(for cell: UITableViewCell, at indexPath: IndexPath) {
-        var isChecked = false
+        let item = items[indexPath.row]
         
-        if indexPath.row == 0 {
-            isChecked = row0item.checked
-        } else if indexPath.row == 1 {
-            isChecked = row1item.checked
-        } else if indexPath.row == 2 {
-            isChecked = row2item.checked
-        } else if indexPath.row == 3 {
-            isChecked = row3item.checked
-        } else if indexPath.row == 4 {
-            isChecked = row4item.checked
-        }
-        
-        if isChecked {
+        if item.checked {
             cell.accessoryType = .checkmark
+            
         } else {
             cell.accessoryType = .none
         }
+//
+//        if indexPath.row == 0 {
+//            isChecked = row0item.checked
+//        } else if indexPath.row == 1 {
+//            isChecked = row1item.checked
+//        } else if indexPath.row == 2 {
+//            isChecked = row2item.checked
+//        } else if indexPath.row == 3 {
+//            isChecked = row3item.checked
+//        } else if indexPath.row == 4 {
+//            isChecked = row4item.checked
+//        }
+//
+//        if isChecked {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
     }
 }
