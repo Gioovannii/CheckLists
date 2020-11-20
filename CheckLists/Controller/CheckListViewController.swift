2377a56c039a295ce03f26ac6b45910088c438c0
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-20 16:10:04 +0800

    Clean up code

--- CheckLists/Controller/CheckListViewController.swift
+++ CheckLists/Controller/CheckListViewController.swift
@@ -59,21 +59,10 @@ class CheckListViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
         configureCell(for: cell, at: indexPath)
-//
-//        if indexPath.row == 0 {
-//            label.text = row0item.text
-//        } else if indexPath.row == 1 {
-//            label.text = row1item.text
-//        } else if indexPath.row == 2 {
-//            label.text = row2item.text
-//        } else if indexPath.row == 3 {
-//            label.text = row3item.text
-//        } else if indexPath.row == 4 {
-//            label.text =  row4item.text
-//        }
-//        configureCell(for: cell, at: indexPath)
+
         return cell
     }
+    
     // MARK: - Table View Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -86,25 +75,7 @@ class CheckListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-//            if indexPath.row == 0 {
-//                row0item.checked.toggle()
-//
-//            } else if indexPath.row == 1 {
-//                row1item.checked.toggle()
-//
-//            } else if indexPath.row == 2 {
-//                row2item.checked.toggle()
-//
-//            } else if indexPath.row == 3 {
-//                row3item.checked.toggle()
-//
-//            } else if indexPath.row == 4 {
-//                row4item.checked.toggle()
-//            }
-//            configureCell(for: cell, at: indexPath)
-//        }
-//        tableView.deselectRow(at: indexPath, animated: true)
-//    }
+
     
     func configureCell(for cell: UITableViewCell, at indexPath: IndexPath) {
         let item = items[indexPath.row]
@@ -115,23 +86,5 @@ class CheckListViewController: UITableViewController {
         } else {
             cell.accessoryType = .none
         }
-//
-//        if indexPath.row == 0 {
-//            isChecked = row0item.checked
-//        } else if indexPath.row == 1 {
-//            isChecked = row1item.checked
-//        } else if indexPath.row == 2 {
-//            isChecked = row2item.checked
-//        } else if indexPath.row == 3 {
-//            isChecked = row3item.checked
-//        } else if indexPath.row == 4 {
-//            isChecked = row4item.checked
-//        }
-//
-//        if isChecked {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
     }
 }
