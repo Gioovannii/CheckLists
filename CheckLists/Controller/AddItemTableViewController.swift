684299014aa4d111ae8e21967c9bd244ed781182
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-21 19:49:50 +0800

    New File to set new table view

---
+++ CheckLists/Controller/AddItemTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddItemTableViewController.swift
+//  CheckLists
+//
+//  Created by Giovanni Gaffé on 2020/11/21.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
