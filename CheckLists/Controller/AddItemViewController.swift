ee866cef82b884cdd5950fc29e799b5b7e89d0d4
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-11-23 19:00:40 +0800

    set textField method with checking if there is a text inside

--- CheckLists/Controller/AddItemViewController.swift
+++ CheckLists/Controller/AddItemViewController.swift
@@ -11,7 +11,10 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
-    override func viewDidLoad() { super.viewDidLoad() }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
@@ -26,5 +29,18 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
         print("Contents of the text field: \(text)")
         
         navigationController?.popViewController(animated: true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return false }
+        let oldText = text
+        guard let stringRange = Range(range, in: oldText) else { return false }
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
 }
