607848fcd7a82cbdcfc8452637c55207c29b49fa
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-05 17:08:12 +0800

    create cellIdentifier use in cellForRowAt

--- CheckLists/AllListsViewController.swift
+++ CheckLists/AllListsViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class AllListsViewController: UITableViewController {
+    
+    // MARK: - Properties
+    let cellIdentifier = "ChecklistCell"
+    
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        guard let textLabel = cell.textLabel else { return UITableViewCell() }
+        textLabel.text = "List \(indexPath.row)"
+        return cell
     }
 }
