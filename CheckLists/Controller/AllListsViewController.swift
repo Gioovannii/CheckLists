016773f052c7b569d1c1d002c053ff8bf7b97ccd
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-10 20:04:08 +0800

    remove comment unused

--- CheckLists/Controller/AllListsViewController.swift
+++ CheckLists/Controller/AllListsViewController.swift
@@ -49,7 +49,6 @@ class AllListsViewController: UITableViewController {
         cell.accessoryType = .detailDisclosureButton
         
 
-//        textLabel.text = "List \(indexPath.row)"
         return cell
     }
     
