e72cf54d42eb307fd28a3ec1067426d65db5d4d5
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-10 19:59:38 +0800

    send checklist object with perform

--- CheckLists/Controller/AllListsViewController.swift
+++ CheckLists/Controller/AllListsViewController.swift
@@ -54,6 +54,7 @@ class AllListsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+        let checklist = lists[indexPath.row]
+        performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
 }
