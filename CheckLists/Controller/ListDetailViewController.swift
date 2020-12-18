a9c38f09e0a633b437dc7a4981c3f893cdf361ae
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-18 14:51:53 +0800

    Set don action now with his delegate

--- CheckLists/Controller/ListDetailViewController.swift
+++ CheckLists/Controller/ListDetailViewController.swift
@@ -50,4 +50,13 @@ final class ListDetailViewController: UITableViewController, UITextFieldDelegate
         delegate?.listDetailViewControllerDidCancel(self)
     }
     
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
 }
