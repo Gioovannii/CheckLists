2114dfcaa0772d37585a69fe4671954e7110d88d
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-21 13:42:42 +0800

    Load and save directory and checklists

--- CheckLists/Controller/AllListsViewController.swift
+++ CheckLists/Controller/AllListsViewController.swift
@@ -39,6 +39,8 @@ class AllListsViewController: UITableViewController {
             item.text = "Item for \(list.name)"
             list.items.append(item)
         }
+        
+        loadChecklists()
     }
     
     // MARK: - Navigation
@@ -92,6 +94,42 @@ class AllListsViewController: UITableViewController {
         
         navigationController?.pushViewController(controller, animated: true)
     }
+    
+    // MARK: - Data saving
+
+        func documentsDirectory() -> URL {
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            return paths[0]
+        }
+    
+        func dataFilePath() -> URL {
+            documentsDirectory().appendingPathComponent("Checklists.plist")
+        }
+    
+        func saveChecklists() {
+            let encoder = PropertyListEncoder()
+    
+            do {
+                let data = try encoder.encode(lists)
+                try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+    
+            } catch {
+                print("Error encoding item array: \(error.localizedDescription)")
+            }
+        }
+    
+        func loadChecklists() {
+            let path = dataFilePath()
+    
+            if let data = try? Data(contentsOf: path) {
+                let decoder = PropertyListDecoder()
+                do {
+                    lists = try decoder.decode([Checklist].self, from: data)
+                } catch {
+                    print("Error decoding item array: \(error.localizedDescription)")
+                }
+            }
+        }
 }
 
 extension AllListsViewController: ListDetailViewControllerDelegate {
