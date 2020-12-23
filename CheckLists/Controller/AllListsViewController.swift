288872ce21a1660fa98ef39d00d2a99d77e23a88
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-23 11:07:47 +0800

    Move documentary to model to save and load

--- CheckLists/Controller/AllListsViewController.swift
+++ CheckLists/Controller/AllListsViewController.swift
@@ -95,42 +95,6 @@ class AllListsViewController: UITableViewController {
         
         navigationController?.pushViewController(controller, animated: true)
     }
-    
-    // MARK: - Data saving
-
-    func documentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func dataFilePath() -> URL {
-        documentsDirectory().appendingPathComponent("Checklists.plist")
-    }
-    
-    func saveChecklists() {
-        let encoder = PropertyListEncoder()
-        
-        do {
-            let data = try encoder.encode(lists)
-            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
-            
-        } catch {
-            print("Error encoding item array: \(error.localizedDescription)")
-        }
-    }
-    
-    func loadChecklists() {
-        let path = dataFilePath()
-        
-        if let data = try? Data(contentsOf: path) {
-            let decoder = PropertyListDecoder()
-            do {
-                lists = try decoder.decode([Checklist].self, from: data)
-            } catch {
-                print("Error decoding item array: \(error.localizedDescription)")
-            }
-        }
-    }
 }
 
 extension AllListsViewController: ListDetailViewControllerDelegate {
