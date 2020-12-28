8389a601cfe51007fcbb9bccf8395207324bafc2
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-28 16:38:27 +0800

    Change view will appear to view did appear

--- CheckLists/Controller/AllListsViewController.swift
+++ CheckLists/Controller/AllListsViewController.swift
@@ -26,8 +26,8 @@ class AllListsViewController: UITableViewController {
         //print("Document folder is \(documentsDirectory())")
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         navigationController?.delegate = self
     }
