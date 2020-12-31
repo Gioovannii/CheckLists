6dba4faa3a3f2ddd8f9f0f7b4a9b226db23391f4
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-31 10:44:49 +0800

    New file set delegate

---
+++ CheckLists/Controller/IconPickerViewController.swift
@@ -0,0 +1,12 @@
+//
+//  IconPickerViewController.swift
+//  CheckLists
+//
+//  Created by Giovanni Gaffé on 2020/12/31.
+//
+
+import UIKit
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+}
