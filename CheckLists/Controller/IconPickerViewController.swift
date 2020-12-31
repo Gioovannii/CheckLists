1b9611915dbed56fd1aefef1d0e1c3917f44db8a
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-12-31 10:56:09 +0800

    set cell for row at to set how cell will be

--- CheckLists/Controller/IconPickerViewController.swift
+++ CheckLists/Controller/IconPickerViewController.swift
@@ -17,8 +17,16 @@ class IconPickerViewController: UITableViewController {
     let icons = [ "No icon", "Appointments", "Birthdays", "Chores",
     "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"]
 
+    // MARK: - Table View delegates
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return icons.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+        let iconName = icons[indexPath.row]
+        cell.textLabel?.text = iconName
+        cell.imageView?.image = UIImage(named: iconName)
+        return cell
+    }
 }
