2620af37a25a00dc86571cee3e0c1c1baa697921
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-02 17:14:26 +0800

    Set checklist with initializer name and iconName

--- CheckLists/Controller/ListDetailViewController.swift
+++ CheckLists/Controller/ListDetailViewController.swift
@@ -52,8 +52,7 @@ final class ListDetailViewController: UITableViewController {
             checklist.iconName = iconName
             delegate?.listDetailViewController(self, didFinishEditing: checklist)
         } else {
-            let checklist = Checklist(name: textField.text!)
-            checklist.iconName = iconName
+            let checklist = Checklist(name: textField.text!, iconName: iconName)
             delegate?.listDetailViewController(self, didFinishAdding: checklist)
         }
     }
