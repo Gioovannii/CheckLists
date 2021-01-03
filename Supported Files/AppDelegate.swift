87c769166cba4526ea208237bdaa4e882c2abf42
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-03 14:15:47 +0800

    remove test notification we know it's working

--- Supported Files/AppDelegate.swift
+++ Supported Files/AppDelegate.swift
@@ -16,24 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //Notification Authorization
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
-            if granted {
-                print("We have permission")
-            } else {
-                print("Permission denied")
-            }
-        }
+//        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+//            if granted {
+//                print("We have permission")
+//            } else {
+//                print("Permission denied")
+//            }
+//        }
         
         center.delegate = self
         
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "I am a local notification"
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
-        center.add(request)
         return true
     }
 
